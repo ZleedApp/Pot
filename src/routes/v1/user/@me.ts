409df9e66a59587f1e637bc4b9333bd81d61ec1c
TheClashFruit <clash.fruit123@gmail.com>
2024-08-02 01:07:09 +0200

    feat: openapi spec for `/v1/user/@me`

--- src/routes/v1/user/@me.ts
+++ src/routes/v1/user/@me.ts
@@ -6,7 +6,15 @@ import { User } from '../../../interfaces/user';
 
 import { verify } from 'jsonwebtoken';
 
+import {
+  OpenApiBuilder,
+  OpenApiMethod,
+  OpenApiResponse
+} from '../../../util/openapi_builder';
+
 import Database from '../../../util/database';
+
+import type { OpenApi } from '../../../util/openapi_builder';
 
 interface ZleedJWT {
   id: bigint;
@@ -15,6 +23,58 @@ interface ZleedJWT {
   iat: number;
   exp: number;
 }
+
+export const openapi = (): OpenApi => {
+  return new OpenApiBuilder()
+    .addMethod(
+      Method.GET,
+      new OpenApiMethod().description('User info.').addResponse(
+        Status.OK,
+        new OpenApiResponse<User>()
+          .description('Ok')
+          .name('User')
+          .schema({
+            type: 'object',
+            properties: {
+              id: { type: 'integer' },
+              username: { type: 'string' },
+              displayName: { type: 'string' },
+              email: { type: 'string' },
+              avatar: { type: 'string' },
+              banner: { type: 'string' },
+              badges: { type: 'array', items: { type: 'integer' } },
+              connections: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'integer' },
+                    url: { type: 'string' },
+                    createdAt: { type: 'integer' }
+                  }
+                }
+              },
+              streamTitle: { type: 'string' },
+              streamKeys: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'integer' },
+                    key: { type: 'string' },
+                    createdAt: { type: 'integer' }
+                  }
+                }
+              },
+              lastLive: { type: 'string' },
+              createdAt: { type: 'integer' },
+              updatedAt: { type: 'integer' }
+            }
+          })
+      )
+    )
+    .build();
+};
 
 export default function handler(req: Request, res: Response<User>) {
   const jwtToken = req.getHeader('Authorization');
